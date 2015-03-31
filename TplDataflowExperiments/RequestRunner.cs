aa41adb6708db1de305e34d94b8fa442760fa46c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-03-31 23:52:15 +0100

    Created a RequestRunner class

---
+++ TplDataflowExperiments/RequestRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace TplDataflowExperiments
+{
+    public static class RequestRunner
+    {
+        public static IEnumerable<TResponse> RunAsyncRequests<TRequest, TResponse>(
+            IEnumerable<TRequest> requests,
+            Func<TRequest, Task<TResponse>> createTask,
+            int maxDegreeOfParallelism = 1)
+        {
+            var options = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = maxDegreeOfParallelism
+            };
+
+            var transformBlock = new TransformBlock<TRequest, TResponse>(createTask, options);
+            var bufferBlock = new BufferBlock<TResponse>();
+            transformBlock.LinkTo(bufferBlock);
+            foreach (var request in requests) transformBlock.Post(request);
+            transformBlock.Complete();
+            transformBlock.Completion.Wait();
+            IList<TResponse> results;
+            bufferBlock.TryReceiveAll(out results);
+            return results;
+        }
+    }
+}
---
+++ TplDataflowExperiments/RequestRunnerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FsCheck;
+using FsCheck.Fluent;
+using NUnit.Framework;
+
+namespace TplDataflowExperiments
+{
+    [TestFixture]
+    public class RequestRunnerTests
+    {
+        [Test]
+        public void Test()
+        {
+            var requests = BuildRequests(50);
+            var responses = RequestRunner.RunAsyncRequests(requests, CreateTask, 5);
+            foreach (var response in responses)
+            {
+                Console.WriteLine("response: index: {0}; ms: {1}", response.Item1, response.Item2);
+            }
+        }
+
+        private static IEnumerable<Tuple<int, int>> BuildRequests(int numRequests)
+        {
+            var g = Any.IntBetween(1, 100).MakeListOfLength(numRequests);
+            var sample = Gen.sample(0, 1, g);
+            var requests = sample.First();
+            return Enumerable.Range(0, int.MaxValue).Zip(requests, Tuple.Create);
+        }
+
+        private static Task<Tuple<int, string>> CreateTask(Tuple<int, int> request)
+        {
+            return Task.Factory.StartNew(
+                state =>
+                {
+                    var tuple = (Tuple<int, int>) state;
+                    var index = tuple.Item1;
+                    var ms = tuple.Item2;
+                    return Tuple.Create(index, Convert.ToString(ms));
+                },
+                request);
+        }
+    }
+}
